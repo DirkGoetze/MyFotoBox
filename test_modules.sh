96b20ae5b02de36ab9a82b58faaf86c021963728
Dirk Götze <goetze.dirk@hotmail.de>
2025-06-24 23:26:19 +0200

    refactor: Deaktiviere lokales Debug-Flag in manage_folders.sh; optimiere Testausgaben in test_modules.sh

--- test_modules.sh
+++ test_modules.sh
@@ -284,14 +284,15 @@ echo "+-----------------------------------------------------------------------+"
 test_function "manage_folders_sh" "get_tmp_dir"
 # Test: get_nginx_systemdir
 echo "+-----------------------------------------------------------------------+"
-echo "| Test: get_nginx_systemdir                                                   |"
+echo "| Test: get_nginx_systemdir                                             |"
 echo "+-----------------------------------------------------------------------+"
 test_function "manage_folders_sh" "get_nginx_systemdir"
 # Test: get_systemd_systemdir
 echo "+-----------------------------------------------------------------------+"
-echo "| Test: get_systemd_systemdir                                                   |"
+echo "| Test: get_systemd_systemdir                                           |"
 echo "+-----------------------------------------------------------------------+"
 test_function "manage_folders_sh" "get_systemd_systemdir"
+
 
 exit
 
