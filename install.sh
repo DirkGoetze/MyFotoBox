b62c7d93ba7b4c1c69f21567e34674ac3901743d
Dirk Götze <goetze.dirk@hotmail.de>
2025-07-01 12:10:55 +0200

    refactor: Deaktiviere lokalen Debug-Modus in install.sh für stabilere Ausführung

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ set +e
 # Debug-Modus: Lokal und global steuerbar
 # DEBUG_MOD_LOCAL: Wird in jedem Skript individuell definiert (Standard: 0)
 # DEBUG_MOD_GLOBAL: Überschreibt alle lokalen Einstellungen (Standard: 0)
-DEBUG_MOD_LOCAL=1            # Lokales Debug-Flag für einzelne Skripte
+DEBUG_MOD_LOCAL=0            # Lokales Debug-Flag für einzelne Skripte
 : "${DEBUG_MOD_GLOBAL:=0}"   # Globales Flag, das alle lokalen überstimmt
 # ---------------------------------------------------------------------------
 # Diese Variablen werden global deklariert und später sicher initialisiert
