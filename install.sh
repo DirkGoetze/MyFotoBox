be88cb821aff3aae49dc3dae01381ef697e2b7f0
Dirk Götze <goetze.dirk@hotmail.de>
2025-07-20 22:44:58 +0200

    feat: aktiviere globale Debug-Option für verbesserte Fehlerdiagnose im Installationsskript

--- install.sh
+++ install.sh
@@ -1084,6 +1084,7 @@ main() {
         fi
     }
 
+    DEBUG_MOD_GLOBAL=1
     # Ausführung der einzelnen Dialogschritte, robuste Fehlerbehandlung
     run_step dlg_check_system_requirements "$@"  # Prüfe Systemvoraussetzungen 
     run_step dlg_prepare_system           # Installiere Systempakete und prüfe Erfolg
