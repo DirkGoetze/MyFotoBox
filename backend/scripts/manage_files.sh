05200f1ca9072ff37414c5c786fbffe023f53edd
Dirk Götze <goetze.dirk@hotmail.de>
2025-07-21 02:36:25 +0200

    feat: verbessere die Zuweisung von DB_FILENAME in get_data_file

--- backend/scripts/manage_files.sh
+++ backend/scripts/manage_files.sh
@@ -210,7 +210,10 @@ get_data_file() {
     full_filename="$(_get_file_name "$file_name" "$file_ext" "$folder_path")"
     if [ $? -eq 0 ] && [ -n "$full_filename" ]; then
         # Erfolg: Datei existiert und ist les-/schreibbar
-        DB_FILENAME="$full_filename"
+        if [ -z "${DB_FILENAME+x}" ] || [ -z "$DB_FILENAME" ] || [ "$full_filename" != "$DB_FILENAME" ]; then
+            DB_FILENAME="$full_filename"
+            export DB_FILENAME
+        fi
         debug "$(printf "$get_data_file_debug_0004" "$full_filename")"
         echo "$full_filename"
         return 0
