64dd3265194b1fe38080247310552356a523b6b8
Dirk Götze <goetze.dirk@hotmail.de>
2025-07-22 20:33:37 +0200

    feat: kommentiere Debug-Modus-Variablen in manage_files.sh und manage_folders.sh aus

--- backend/scripts/manage_files.sh
+++ backend/scripts/manage_files.sh
@@ -64,8 +64,8 @@ BACKUP_FILE_EXT_DEFAULT=".bak" #'.zip' ist nicht mehr Standard, da wir keine ZIP
 # Debug-Modus: Lokal und global steuerbar
 # DEBUG_MOD_LOCAL: Wird in jedem Skript individuell definiert (Standard: 0)
 # DEBUG_MOD_GLOBAL: Überschreibt alle lokalen Einstellungen (Standard: 0)
-DEBUG_MOD_LOCAL=0            # Lokales Debug-Flag für einzelne Skripte
-: "${DEBUG_MOD_GLOBAL:=0}"   # Globales Flag, das alle lokalen überstimmt
+#DEBUG_MOD_LOCAL=0            # Lokales Debug-Flag für einzelne Skripte
+#: "${DEBUG_MOD_GLOBAL:=0}"   # Globales Flag, das alle lokalen überstimmt
 # ---------------------------------------------------------------------------
 
 # _get_file_name
